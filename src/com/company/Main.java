af5a5999bf78c9894dcbb7c74109f20a67245059
Админ <dimashunk_o@mail.ru>
2021-02-18 19:37:37 +0300

    Commit 1

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   byte num = 2;
+	   System.out.println(num);
+	   int i = 234654;
+	   System.out.println(i);
+	   long l = 14324423;
+	   System.out.println(l);
+	   float n = 34.54f;
+	   System.out.println(n);
+	   double n2;
+	   n2 = 2342.24f;
+	   System.out.println(n2);
+	   char sym = 'H';
+	   System.out.println(sym);
+       boolean is_happy = true;
+       System.out.println(is_happy);
+       String str = "Hello Word!!!";
+       System.out.println(str);
+    }
+}
